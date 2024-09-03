95ca4d0232673e1857bbcd06e44b7b0731c4aaef
sugamsharmaIt <sugamsharma0007@gmail.com>
2024-09-03 17:47:48 +0530

    Add TPA for login

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -123,7 +123,10 @@ namespace DivyangCareerApi.Controllers
                 param.Add("Mode", 3);
                 param.Add("Email", email);
                 param.Add("Password", password);
+                param.Add("Result", dbType: DbType.String, size: 200, direction: ParameterDirection.Output);
                 message.Data = await Task.FromResult(_dapper.Get<EmployerDetails>("Usp_Employee", param));
+                var status = Convert.ToString(param.Get<string>("@Result"));
+                message.ReturnMessage = status;
             }
             catch (Exception ex)
             {
@@ -169,6 +172,100 @@ namespace DivyangCareerApi.Controllers
             else
             {
                 message.IsSuccess = true;
+                message.ReturnMessage = message.ReturnMessage;
+            }
+            return message;
+        }
+        [HttpGet(nameof(GetEmployerDetails))]
+        public async Task<Message<EmployeeDetails>> GetEmployerDetails(string email)
+        {
+            Message<EmployeeDetails> message = new Message<EmployeeDetails>();
+            var param = new DynamicParameters();
+            try
+            {
+                param.Add("Mode", 7);
+                param.Add("Email", email);
+                message.Data = await Task.FromResult(_dapper.Get<EmployeeDetails>("Usp_Employee", param));
+            }
+            catch (Exception ex)
+            {
+                message.ReturnMessage = ex.Message;
+            }
+            if (message.Data != null)
+            {
+                message.IsSuccess = true;
+                message.ReturnMessage = "Success";
+            }
+            else
+            {
+                message.IsSuccess = true;
+                message.ReturnMessage = "InvalidEmailOrPassword";
+            }
+            return message;
+        }
+        [HttpPost(nameof(UpdateEmployerDetails))]
+        public async Task<Message<int>> UpdateEmployerDetails([FromBody] EmployeeDetails Model)
+        {
+            Message<int> message = new Message<int>();
+            var pram = new DynamicParameters();
+            try
+            {
+                pram.Add("Mode", 10);
+                pram.Add("FirstName", Model.Firstnames);
+                pram.Add("LastName", Model.Lastnames);
+                pram.Add("Email", Model.Email);
+                pram.Add("PhoneNumber", Model.PhoneNumber);
+                pram.Add("CountryCode", Model.CountryCode);
+                pram.Add("ProfileImage", Model.ImageName);
+                pram.Add("@EmployeeIds", Model.EmployeeId);
+                pram.Add("Result", dbType: DbType.String, size: 200, direction: ParameterDirection.Output);
+                message.Data = await Task.FromResult(_dapper.Post<int>("Usp_Employee", pram));
+                var status = Convert.ToString(pram.Get<string>("@Result"));
+                message.ReturnMessage = status;
+            }
+            catch (Exception ex)
+            {
+                message.ReturnMessage = ex.Message;
+            }
+            if (message.Data == 1)
+            {
+                message.IsSuccess = true;
+                message.ReturnMessage = message.ReturnMessage;
+            }
+            else
+            {
+                message.IsSuccess = true;
+                message.ReturnMessage = message.ReturnMessage;
+            }
+            return message;
+        }
+
+        [HttpPost(nameof(CheckTPAUser))]
+        public async Task<Message<int>> CheckTPAUser([FromBody] CheckEmail Model)
+        {
+            Message<int> message = new Message<int>();
+            var pram = new DynamicParameters();
+            try
+            {
+                pram.Add("Mode", 9);
+                pram.Add("Email", Model.Email);
+                pram.Add("Result", dbType: DbType.String, size: 200, direction: ParameterDirection.Output);
+                message.Data = await Task.FromResult(_dapper.Post<int>("Usp_Employee", pram));
+                var status = Convert.ToString(pram.Get<string>("@Result"));
+                message.ReturnMessage = status;
+            }
+            catch (Exception ex)
+            {
+                message.ReturnMessage = ex.Message;
+            }
+            if (message.Data == 1)
+            {
+                message.IsSuccess = true;
+                message.ReturnMessage = message.ReturnMessage;
+            }
+            else
+            {
+                message.IsSuccess = false;
                 message.ReturnMessage = message.ReturnMessage;
             }
             return message;
---
+++ Models/EmployeeDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DivyangCareerApi.Models
+{
+    public class EmployeeDetails
+    {
+        public string EmployeeId { get; set; }
+        public string Firstnames { get; set; }
+        public string Lastnames { get; set; }
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string ProfileImagePath { get; set; }
+        public string ImageName { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
